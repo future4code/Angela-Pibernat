e1a082618014a13bfc72607a735a65539d430c97
unknown <angela.pibernat@gmail.com>
2021-10-07 21:45:12 -0300

    Resolução dos exercícios 6 ao 9

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -80,40 +80,117 @@ function retornaExpressoesBooleanas() {
 
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
-
+  let numerosPares = []
+  for (let i=0; numerosPares.length<n; i++){
+    if (i%2 === 0){
+    numerosPares.push(i)
+    }
+  }
+  return numerosPares
 }
+
 
 // EXERCÍCIO 08
 function checaTriangulo(a, b, c) {
-  // return 'Escaleno'
-  // return 'Equilátero'
-  // return 'Isósceles'
-}
+  if (a===b && b===c){
+  return 'Equilátero'
+  } 
+  if (a===b || a===c || b===c){
+  return 'Isósceles'
+  }
+  if (a!==b && b!==c && c!==a){
+  return 'Escaleno'
+}}
 
 // EXERCÍCIO 09
 function comparaDoisNumeros(num1, num2) {
-  // Formato do objeto a ser retornado:
-  // {
-  //   maiorNumero: X,
-  //   maiorDivisivelPorMenor: Y,
-  //   diferenca: Z
-  // }
+  let X
+  let Y
+  let Z
+ if (num1>num2){
+   X=num1
+ }else{ X=num2}
+
+ if (num1%num2===0 || num2%num1===0){
+  Y=true
+ }else{Y=false}
+
+Z=Math.abs(num2-num1)
+
+  const objeto = {
+    maiorNumero: X,
+    maiorDivisivelPorMenor: Y,
+    diferenca: Z
+  }
+  return objeto
 }
 
 // EXERCÍCIO 10
+
+//let array = [1,2,3,4,5,6,7]
+//let arra = []
+
 function segundoMaiorEMenor(array) {
 
-}
+  // arrayNova = []
+  // let segundoMaior
+  // let segundoMenor 
+
+  // for (let i=0 ; i<array.length ; i++){
+
+  //     if (array[i]<array[i+1] ){
+  //     segundoMaior = array[i]
+      
+  //     }
+      
+  //   }    
+  // for (let i = array.length ; i>0 ; i--){  
+  //   if (array[i]>array[i-1]){
+  //     segundoMenor = array[i]
+  //   }
+  //   }
+  //   arrayNova.push(segundoMaior) 
+  //   arrayNova.push(segundoMenor)
+  //   return arrayNova
+  }
+  
+
+
+
+
 
 // EXERCÍCIO 11
+
 function ordenaArray(array) {
 
+  // let novoArray = []
+
+  // for (item of array){
+  //   let contador = 0
+  //   for (itemComparado of array){
+  //     if(item > itemComparado){
+  //       contador ++
+  //     }
+  //   }
+  //   novoArray[contador]=item
+  // }
+  // return novoArray
 }
+  
 
 // EXERCÍCIO 12
-function filmeFavorito() {
 
+function filmeFavorito() {
+//     const objeto = {
+//     nome: "O Diabo Veste Prada",
+//     ano:2006,
+//     diretor: "David Frankel",
+//     atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+//    }
+// console.log (filmeFavorito())   
+// return objeto
 }
+
 
 // EXERCÍCIO 13
 function imprimeChamada() {
